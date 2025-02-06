145a0dba66b0396f2908911ff9254de562307839
Kevin Weber <weberkm5@gmail.com>
2025-02-06 14:18:57 -0600

    register user

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,42 +1,26 @@
-const cohort = "2303-FTB-MT-WEB-FT";
+const cohort = "2211-FTB-ET-WEB-FT";
 const BASE_URL = `https://strangers-things.herokuapp.com/api/${cohort}`;
 
 export const registerUser = async (username, password) => {
   try {
-    const response = await fetch(`${BASE_URL}/users/register`, {
+    const response = await fetch(
+      `${BASE_URL}/users/register`, {
       method: "POST",
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json'
       },
       body: JSON.stringify({
         user: {
           username,
-          password,
-        },
-      }),
+          password
+        }
+      })
     });
-    const {
-      data: { token },
-    } = await response.json();
-
+    const {data:{token}} = await response.json();
+ 
+    // console.log(token,'this is token in reguser');
     return token;
   } catch (error) {
     console.error(error);
   }
-};
-
-export const fetchMe = async (token) => {
-  try {
-    const response = await fetch(`${BASE_URL}/users/me`, {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    const result = await response.json();
-    console.log(result);
-    return result;
-  } catch (error) {
-    console.error(error);
-  }
-};
+}
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,39 +1,112 @@
-import React, { useState } from "react";
-import { registerUser } from "../api/auth";
-const Register = ({ setToken }) => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    // call some function send it the username and password
-    // we just created and then that function will take that
-    // information and create a new user in the backend.
-    const token = await registerUser(username, password);
+import React from 'react';
+import { useState } from 'react';
+import { registerUser } from '../api/auth';
 
-    localStorage.setItem("token", token);
-    setToken(token);
-    console.log(username, password);
-  };
+const Register = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          onChange={(e) => setUsername(e.target.value)}
-          value={username}
-          type="text"
-          placeholder="username"
-        ></input>
-        <input
-          onChange={(e) => setPassword(e.target.value)}
-          value={password}
-          type="password"
-          placeholder="password"
-        ></input>
-        <button type="submit">Register</button>
+      <form
+        onSubmit={async (e)=>{
+          try {
+            e.preventDefault();
+            const token = await registerUser(username, password);
+            console.log(token,'in insubmit this is token');
+            setUsername('');
+            setPassword('');
+          } catch (error) {
+            console.error(error);
+          }
+        }}
+      >
+        <input 
+          value={username} 
+          type='text' 
+          placeholder='username'
+          onChange={(e)=>{
+            setUsername(e.target.value)
+          }}
+          ></input>
+        <input 
+          value={password} 
+          type='password' 
+          placeholder='password'
+          onChange={(e)=>{
+            setPassword(e.target.value)
+          }}
+          ></input>
+        <button 
+          type='submit'
+        >Submit!</button>
       </form>
     </div>
   );
 };
 
 export default Register;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useState } from "react";
+// import { registerUser } from "../api/auth";
+// const Register = ({ setToken }) => {
+//   const [username, setUsername] = useState("");
+//   const [password, setPassword] = useState("");
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     // call some function send it the username and password
+//     // we just created and then that function will take that
+//     // information and create a new user in the backend.
+//     const token = await registerUser(username, password);
+
+//     localStorage.setItem("token", token);
+//     setToken(token);
+//     console.log(username, password);
+//   };
+
+//   return (
+//     <div>
+//       <form onSubmit={handleSubmit}>
+//         <input
+//           onChange={(e) => setUsername(e.target.value)}
+//           value={username}
+//           type="text"
+//           placeholder="username"
+//         ></input>
+//         <input
+//           onChange={(e) => setPassword(e.target.value)}
+//           value={password}
+//           type="password"
+//           placeholder="password"
+//         ></input>
+//         <button type="submit">Register</button>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default Register;
