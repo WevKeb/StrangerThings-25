6e68bc55515bb2ac8ed4055113ba35e0b0938157
Kevin <weberkm5@gmail.com>
2025-07-30 21:22:00 -0500

    added item details modal

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,19 +1,45 @@
 import React from 'react';
 import { fetchAllPosts } from '../api/auth';
+import { useState } from 'react';
 import './PostsList.css'
 
-const PostsList = ({posts, setPosts}) => {
-    
+const PostsList = ({ posts, setPosts }) => {
+    const [selectedPost, setSelectedPost] = useState(null);
+
     return (
         <div className="post-card-cnt">
-            {posts.map((post)=>
+            {posts.map((post) =>
                 <div key={post._id} className="post-card">
                     <h2>{post.title}</h2>
                     <p>{post.description}</p>
                     <p>Price: {post.price}</p>
-                    <button className="card-button">More Info</button>
+                    <button
+                        className="card-button"
+                        onClick={() => {
+                            // console.log(user);
+                            setSelectedPost(post);
+
+                        }}
+                    >More Info</button>
                 </div>
-                
+            )}
+
+            {selectedPost && (
+                <div className='modal-backdrop'>
+                    <div className='modal'>
+                        <h2>Title: {selectedPost.title}</h2>
+                        <h5>Listed By: {selectedPost.author.username}</h5>
+                        <div>
+                            Will Deliver: {selectedPost.willDeliver ? 'Yes' : 'No'}
+                        </div>
+                        <div>Location: {selectedPost.location}</div>
+                        <div>Description: {selectedPost.description}</div>
+                        <button
+                            onClick={() => {
+                                setSelectedPost(null)
+                            }}>Close</button>
+                    </div>
+                </div>
             )}
         </div>
     );
